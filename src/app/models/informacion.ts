b2427c408f2b67e6ea16948e42474bd928b5fbe6
Sebastian Mosquera <sebastian.mosquera@supergiros.com.co>
2023-01-20 21:53:02 -0500

    se crean los endpoints y los models @Author Sebastian Mosquera

---
+++ src/app/models/informacion.ts
@@ -0,0 +1,12 @@
+/** 
+* @class informacion 
+* @description clase que modela el response de la informacion del personaje
+* @author Sebastian Mosquera
+* @date 20/01/2023
+*/
+
+export interface Informacion {
+    characters: string;
+    episodes: string;
+    locations: string;
+}
