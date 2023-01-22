e0b5657960ed1fe86e4f60233aca1b070898faba
Sebastian Mosquera <sebastian.mosquera@supergiros.com.co>
2023-01-22 12:52:36 -0500

    se crea logica para boton iniciar sesion con disabled y ruteo @Author Sebastian Mosquera Mazud

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +10,34 @@ export class LoginComponent implements OnInit {
 
   title = '¡Bienvenido a la herramienta de registro Coink!';
   subTitle= 'Coink para tu día a día. Empieza ahorrando y cumple tus sueños con propósito.';
+  user;
+  password;
+  userText = false;
+  passwordText= false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  userInput(){
+    if(this.user.length > 0){
+      this.userText = true;
+    }else{
+      this.userText = false;
+    }
+  }
+
+  userPass(){
+    if(this.password.length > 0){
+      this.passwordText = true;
+    }else{
+      this.passwordText = false;
+    }
+  }
+
+  goHome(){
+    this.router.navigateByUrl('/home');
   }
 
 }
